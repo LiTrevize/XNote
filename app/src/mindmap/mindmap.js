3a987a6587f5a38d988fdc1e78fea9b6d3b78e14
Du Liu <gavin_liudu@163.com>
2019-05-17 21:06:53 +0800

    status

---
+++ app/src/mindmap/mindmap.js
@@ -0,0 +1,15 @@
+const d3 = require('d3');
+require('../../node_modules/markmap/lib/d3-flextree');
+const markmap = require('../../node_modules/markmap/lib/view.mindmap');
+const parse = require('../../node_modules/markmap/lib/parse.markdown');
+const transform = require('../../node_modules/markmap/lib/transform.headings');
+
+d3.text("gtor.md", function(error, text) {
+  if (error) throw error;
+  const data = transform(parse(text));
+
+  markmap('svg#mindmap', data, {
+    preset: 'colorful', // or default
+    linkShape: 'diagonal' // or bracket
+  });
+});
